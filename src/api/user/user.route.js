80ca82354e182503df4b6b5f2ef9d81a2457a383
hoangbkcntt98 <bugger485@gmail.com>
2021-11-16 12:12:50 +0700

    change url call back

--- src/api/user/user.route.js
+++ src/api/user/user.route.js
@@ -31,7 +31,7 @@ router.get('/auth/google/callback',
 router.get('/auth/facebook',
     passport.authenticate('facebook',
         {
-            scope: ['email', 'profile'],
+            scope:'email',
         }));
 router.get('/auth/facebook/callback',
     passport.authenticate('facebook', {
