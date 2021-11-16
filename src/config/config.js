8402b02102cabe43484e4a1878829ecd8e58925a
hoangbkcntt98 <bugger485@gmail.com>
2021-11-16 11:12:53 +0700

    change url call back

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 export const GOOGLE_CLIENT_ID = "91289831659-577dafpsmu6dugnlsdj2c2gb0s7abavf.apps.googleusercontent.com";
 export const GOOGLE_CLIENT_SECRET = "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"
-export const GOOGLE_CALLBACK_URL = "http://localhost:5000/user/auth/google/callback"
+export const GOOGLE_CALLBACK_URL = "https://minishop-node-server.herokuapp.com/user/auth/google/callback"
 export const SECRET_CODE = "Minishop"
 export const FACEBOOK_APP_ID = "3185688561448220"
 export const FACEBOOK_APP_SECRET = "876bf25653fa5a2450623149f4f958ae"
-export const FACEBOOK_CALLBACK_URL = "http://localhost:5000/user/auth/facebook/callback"  
+export const FACEBOOK_CALLBACK_URL = "https://minishop-node-server.herokuapp.com/user/auth/facebook/callback"  
